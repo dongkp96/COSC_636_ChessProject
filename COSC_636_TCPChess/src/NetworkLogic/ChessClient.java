a40e42887d99358eab860dad373a58ae6a4438c4
kd pc <dongkp96@gmail.com>
2026-04-28 06:52:28 -0400

    Adds String[] args to main() method of ChessClient

--- COSC_636_TCPChess/src/NetworkLogic/ChessClient.java
+++ COSC_636_TCPChess/src/NetworkLogic/ChessClient.java
@@ -8,7 +8,7 @@ import java.net.Socket;
 import java.util.Scanner;
 
 public class ChessClient {
-    public static void main() {
+    public static void main(String[] args) {
         try{
             Scanner input = new Scanner(System.in);
             //used to handle input from the terminal for the player
